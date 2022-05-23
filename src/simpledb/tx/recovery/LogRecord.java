21756e8127e3ca4d7ab995d97b0f2b24bc22d61e
sunhuashan <2046211182@qq.com>
2022-05-23 21:11:43 +0800

    完善LogRecord接口；添加SetStringRecord类

--- src/simpledb/tx/recovery/LogRecord.java
+++ src/simpledb/tx/recovery/LogRecord.java
@@ -1,10 +1,18 @@
 package simpledb.tx.recovery;
 
+import java.io.IOException;
+
 /**
- * @Author: shs
- * @Data: 2022/5/23 19:05
+ * 日志记录接口
+ *
+ *
+ * @author shs
+ * @date 2022/5/23 19:05
  */
 public interface LogRecord {
+    /**
+     * 不同的常量对应不同类型的日志记录
+     */
     static final int CHECKPOINT = 0;
     static final int START = 1;
     static final int COMMIT = 2;
@@ -12,8 +20,41 @@ public interface LogRecord {
     static final int SET_INT = 4;
     static final int SET_STRING = 5;
 
-    public void writeToLog();
-    public int operation();
+    /**
+     * 将日志记录写入日志文件中
+     *
+     *
+     * @return
+     * 日志记录对应的 LSN
+     *
+     * @throws IOException
+     * I/O异常
+     */
+    public int writeToLog();
+
+    /**
+     * 获取当前日志记录的操作类型
+     *
+     *
+     * @return
+     * 代表日志记录类型的整型变量
+     */
+    public int operator();
+
+    /**
+     * 获取当前日志记录对应的事务号
+     *
+     *
+     * @return
+     * 事务号
+     */
     public int txNumber();
+
+    /**
+     * 撤销该日志记录的操作
+     * <p>
+     * 仅对SET_INT,SET_STRING操作的日志记录生效
+     * </p>
+     */
     public void undo();
 }
---
+++ src/simpledb/tx/recovery/SetStringRecord.java
@@ -0,0 +1,94 @@
+package simpledb.tx.recovery;
+
+import simpledb.buffer.Buffer;
+import simpledb.buffer.BufferManager;
+import simpledb.file.Block;
+import simpledb.log.BasicLogRecord;
+import simpledb.server.SimpleDB;
+
+import java.io.IOException;
+
+/**
+ * 写字符串数据对应的格式化日志记录，日志格式为：
+ * <p>
+ * 【SET_STRING（5），事务号，日志文件名，文件块号，块内偏移，字符串数据】
+ * </p>
+ *
+ *
+ * @author shs
+ * @date 2022/5/23 19:20
+ */
+public class SetStringRecord implements LogRecord{
+
+    private int txNum;
+    private int offset;
+    private String val;
+    private Block blk;
+
+    /**
+     * 构造函数
+     *
+     *
+     * @param txNum
+     * 事务号
+     *
+     * @param offset
+     * 偏移量
+     *
+     * @param val
+     * 字符串变量
+     *
+     * @param blk
+     * 物理块引用{文件名，文件块号}
+     */
+    public SetStringRecord(int txNum, int offset, String val, Block blk) {
+        this.txNum = txNum;
+        this.offset = offset;
+        this.val = val;
+        this.blk = blk;
+    }
+
+    /**
+     * 以BasicLogRecord对象为参数的构造函数
+     *
+     *
+     * @param basicLogRec
+     * 构造参数
+     */
+    public SetStringRecord(BasicLogRecord basicLogRec) {
+        int operator = basicLogRec.nextInt();
+        this.txNum = basicLogRec.nextInt();
+        this.blk = new Block(basicLogRec.nextString(), basicLogRec.nextInt());
+        this.offset = basicLogRec.nextInt();
+        this.val = basicLogRec.nextString();
+    }
+
+    @Override
+    public int writeToLog() {
+        Object[] rec = new Object[]{SET_STRING, txNum, blk.getFilename(), blk.getBlkNum(), offset, val};
+        return SimpleDB.logManager().append(rec);
+    }
+
+    @Override
+    public int operator() {
+        return SET_STRING;
+    }
+
+    @Override
+    public int txNumber() {
+        return txNum;
+    }
+
+    @Override
+    public void undo() {
+        BufferManager bMg = SimpleDB.bufferManager();
+        Buffer buffer = bMg.pin(blk);
+        buffer.setString(offset, val, txNum, -1);
+    }
+
+    @Override
+    public String toString() {
+        return "<SET_STRING " + txNum + " " + blk.getFilename() + " "
+                + offset + " " + val + ">";
+    }
+}
