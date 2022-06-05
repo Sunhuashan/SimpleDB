f51f6e64d6af562bbcb702ceadc02a9455c2c86b
sunhuashan <2046211182@qq.com>
2022-06-05 19:54:45 +0800

    完善注释

--- src/simpledb/tx/concurrency/ConcurrencyManager.java
+++ src/simpledb/tx/concurrency/ConcurrencyManager.java
@@ -6,10 +6,19 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ConcurrencyManager {
+    //所有事务的共享锁表
     private static LockTable lockTbl = new LockTable();
+    //当前事务所持有的锁表
     private Map<Block, String> locks = new HashMap<>();
 
 
+    /**
+     * 为事务请求指定块的共享锁 事务不拥有该块任何锁时才能请求成功
+     *
+     *
+     * @param blk
+     * 指定的块
+     */
     public void sLock(Block blk) {
         if (null == locks.get(blk)) {
             locks.put(blk, "S");
@@ -17,6 +26,15 @@ public class ConcurrencyManager {
         }
     }
 
+    /**
+     * 为事务请求指定块的互斥锁
+     * <p>
+     * 规定事务拥有指定块的共享锁后才能获取该块的互斥锁
+     *
+     *
+     * @param blk
+     * 指定的块
+     */
     public void xLock(Block blk) {
         if (!hasXLock(blk)) {
             sLock(blk);
@@ -25,12 +43,25 @@ public class ConcurrencyManager {
         }
     }
 
+    /**
+     * 释放事务拥有的所有锁
+     */
     public void release() {
         for (Block block : locks.keySet())
             lockTbl.unlock(block);
         locks.clear();
     }
 
+    /**
+     * 判断事务是否拥有指定块的互斥锁
+     *
+     *
+     * @param blk
+     * 指定的块
+     *
+     * @return
+     * true or false
+     */
     boolean hasXLock(Block blk) {
         String type = locks.get(blk);
         return (type != null) && type.equals("X");
